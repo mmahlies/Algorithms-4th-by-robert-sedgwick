370f2705ee8195082d0c3404553f66b104144bd4
mmahlies <eng.m.mahlies@gmail.com>
2021-06-12 13:24:01 +0200

    Commit previous solution Ch1- Ch2

---
+++ Ch2/Exercises/2.5.20/Excersie/Excersie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Excersie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
